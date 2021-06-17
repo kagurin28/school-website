f122c4635a2c14ebe32c8c6cea8e3b67b6a32106
epikgamer696969 <84483210+epikgamer696969@users.noreply.github.com>
2021-06-17 10:39:58 +1000

    Nice

---
+++ streetview/config.js
@@ -0,0 +1,62 @@
+var loadUp_NE_B = function() {
+	pannellum.loadScene("Up-NE", -5, 47.5, 110)
+};
+
+panoramas = `{
+	"default": {
+		"firstScene": "Up-NE",
+		"sceneFadeDuration": 1000,
+		"autoLoad" : true
+	},
+
+	"scenes": {
+		"Up-NE": {
+			"type": "equirectangular",
+			"panorama": "/streetview/images/Up-NE.jpg",
+			
+			"yaw": -50,
+			"pitch": -10,
+			"hotSpots": [
+				{
+					"pitch": -5,
+					"yaw": -47.5,
+					"type": "scene",
+					"sceneId": "Up-NCenE"
+				}
+			]
+		},
+
+		"Up-NCenE": {
+			"type": "equirectangular",
+			"panorama": "/streetview/images/Up-NCenE.jpg",
+			"hotSpots": [
+				{
+					"pitch": -9,
+					"yaw": 177,
+					"type": "scene",
+					"sceneId": "",
+					"clickHandlerFunction": loadUp-NE_B
+				},
+				{
+					"pitch": 2,
+					"yaw": -3.5,
+					"type": "scene",
+					"sceneId": "Up-NCenW"
+				}
+			]
+		},
+		
+		"Up-NCenW": {
+			"type": "equirectangular",
+			"panorama": "/streetview/images/Up-NCenW.jpg",
+			"hotSpots": [
+				{
+					"yaw": 172.5,
+					"pitch": -6,
+					"type": "scene",
+					"sceneId": "Up-NCenE"
+				}
+			]
+		}
+	}
+}`;
