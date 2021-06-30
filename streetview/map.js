4b452ae382f2c573ef3bb02fa0eeb237b4c3f67f
epikgamer696969 <robeyoliverthirtytwo@gmail.com>
2021-07-01 00:33:45 +1000

    Fixed images + finished basic map

--- streetview/map.js
+++ streetview/map.js
@@ -1,6 +1,21 @@
 const mapBtn = document.getElementById("mapBtn");
 const map = document.getElementById("map");
+const mapImg = map.getElementsByTagName("img")[0];
 const panorama = document.getElementById("panorama");
+
+const regExCheckDown = /Down/;
+const regExCheckUp = /Up/;
+
+const circles = Array.from(document.getElementById("mapIcons").getElementsByTagName("circle"));
+var circlesUp = [];
+var circlesDown = [];
+circles.forEach(function(a) { // Sorts circles into seperate Up and Down lists
+	if(regExCheckUp.test(a.id)) {
+		circlesUp.push(a);
+	} else if(regExCheckDown.test(a.id)) {
+		circlesDown.push(a);
+	}
+});
 
 function showMap() {
 	map.style.display = "block";
@@ -21,18 +36,30 @@ function hideMap() {
 
 function updateMarkers() {
 	const scene = viewer.getScene();
-	const circles = document.getElementById("mapIcons").getElementsByTagName("circle");
 	const redCircle = document.getElementById("Mk" + scene);
-	Array.from(circles).forEach(function (a) { // Remove the markerRed class from all circles
+	circles.forEach(function(a) { // Remove the markerRed class from all circles
 		a.classList.remove("markerRed");
 	});
 	redCircle.classList.add("markerRed"); // Add the markerRed class to the red cirle
 	
-	const regExCheckDown = /Down/;
-	if(regExCheckDown.test(scene)) {
-		map.getElementsByTagName("img")[0].src = "/streetview/mapDown.jpg";
-	} else {
-		map.getElementsByTagName("img")[0].src = "/streetview/mapUp.jpg";
+	if(regExCheckUp.test(scene)) { // Change the actual map and show + hide icons
+		mapImg.src = "/streetview/mapUp.jpg";
+		
+		circlesUp.forEach(function(a) {
+			a.style.display = "block";
+		});
+		circlesDown.forEach(function(a) {
+			a.style.display = "none";
+		});
+	} else if(regExCheckDown.test(scene)) {
+		mapImg.src = "/streetview/mapDown.jpg";
+		
+		circlesDown.forEach(function(a) {
+			a.style.display = "block";
+		});
+		circlesUp.forEach(function(a) {
+			a.style.display = "none";
+		});
 	}
 }
 viewer.on("scenechange", function() {updateMarkers();}); // Don't even ask me why I need to wrap this in an anonymous function
