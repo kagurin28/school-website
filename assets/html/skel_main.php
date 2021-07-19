b6a1a8f5df2ab070d25b1647c02ca586e7f0ec43
cheeseburger <cheeseburger@debian-desktop.cheeseburger.local>
2021-07-19 22:58:16 +1000

    Alllllllmost done

--- assets/html/skel_main.php
+++ assets/html/skel_main.php
@@ -3,15 +3,15 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="icon" type="image/png" href="/icon.png">
-	<link rel="stylesheet" href="/css/main.css"></link>
-	<?php echo $_SESSION["head"]; ?>
+	<style><?=$_SESSION["cssMain"]?></style> <!-- The main css file -->
+	<?php include($_SESSION["head"]); ?> <!-- It must include() this as it may have PHP -->
+	<!-- Also, if you are looking at these comments in browser inspect and have no idea what they mean, they're intended for people looking at the actual files so yea :/ -->
 </head>
 <body>
 	<div class="page">
 		<div class="navbar">
 			<a class="navItem logo" href="/">
-				<img src="/icon.png" class="logo"></img>
+				<img src="/favicon.ico" class="logo"></img>
 			</a>
 			<a class="navItem" href="/streetview">Street View</a>
 			<a class="navItem" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Bonus Content</a>
@@ -25,7 +25,7 @@
 			</div>
 		</div>
 		<div class="content">
-			<?php echo $_SESSION["content"]; ?>
+			<?php include($_SESSION["content"]); ?> <!-- Likewise with this one -->
 		</div>
 	</div>
 </body>
--- public/index.php
+++ public/index.php
@@ -2,20 +2,28 @@
 
 session_start();
 
+$_SESSION["cssMain"] = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/../assets/css/main.css"); // Sets the main css file
+
 switch ($_GET["page"]) { // Essentially picks which site you wanted to load and sets variables appropriatly
 	case "home":
-		$_SESSION["head"] = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/../assets/html/head_home.html");
-		$_SESSION["content"] = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/../assets/html/cnt_home.html");
+		$_SESSION["head"] = $_SERVER["DOCUMENT_ROOT"] . "/../assets/html/head_home.html";
+		$_SESSION["content"] = $_SERVER["DOCUMENT_ROOT"] . "/../assets/html/cnt_home.html";
 		break;
 	
 	case "about":
-		$_SESSION["head"] = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/../assets/html/head_about.html");
-		$_SESSION["content"] = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/../assets/html/cnt_about.html");
+		$_SESSION["head"] = $_SERVER["DOCUMENT_ROOT"] . "/../assets/html/head_about.html";
+		$_SESSION["content"] = $_SERVER["DOCUMENT_ROOT"] . "/../assets/html/cnt_about.html";
 		break;
 
 	case "streetview":
-		$_SESSION["head"] = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/../assets/html/head_streetview.html");
-		$_SESSION["content"] = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/../assets/html/cnt_streetview.html");
+		$_SESSION["head"] = $_SERVER["DOCUMENT_ROOT"] . "/../assets/html/head_streetview.php";
+		$_SESSION["content"] = $_SERVER["DOCUMENT_ROOT"] . "/../assets/html/cnt_streetview.php";
+		
+		$_SESSION["cssPnl"] = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/../assets/css/streetview/pannellum.css"); // Yes you have to do this for every individual file, it's painful
+		$_SESSION["cssStv"] = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/../assets/css/streetview/streetview.css");
+		$_SESSION["scptPannellum"] = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/../assets/streetview/pannellum.js");
+		$_SESSION["scptConfig"] = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/../assets/streetview/config.js");
+		$_SESSION["scptMap"] = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/../assets/streetview/map.js");
 		break;
 
 	default:
@@ -23,6 +31,6 @@ switch ($_GET["page"]) { // Essentially picks which site you wanted to load and 
 }
 
 
-include($_SERVER["DOCUMENT_ROOT"] . "/../assets/html/skel_main.html"); // Includes the skeleton
+include($_SERVER["DOCUMENT_ROOT"] . "/../assets/html/skel_main.php"); // Includes the skeleton
 
 ?>
