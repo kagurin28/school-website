d2cc5ba7980dcbf5ddd3f139c7cef327960bad43
cheeseburger <cheeseburger@debian-desktop.cheeseburger.local>
2021-07-27 22:18:21 +1000

    Finished the 'rickroll' aka random videos feature (try clicking on "Bonus Content")

--- src/rickroll.php
+++ src/rickroll.php
@@ -1,17 +1,34 @@
 <?php
 
-$mysqli = new mysqli("localhost", "root", "gitsucks", "nths");
+/* 
 
+This script will send back a link to a random youtube video. It is normally
+prompted with the "Bonus Content" link on the front end. The database that
+stores these links is BonusContent.
+
+*/
+
+$mysqli = new mysqli("localhost", "root", "kazooky", "nths");
+
+// Connection error detection
 if ($mysqli -> connect_errno) {
     echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
     exit();
 }
 
-$result = $mysqli -> query("SELECT url FROM BonusContent");
-while($row = $result->fetch_assoc()) {
-    echo "link: ". $row["url"] . "<br>";
-}
+$query = "SELECT url FROM BonusContent;";
+$result = $mysqli -> query($query);
 
-$mysql -> close;
+// Generates random number from the number of rows
+$numRows = $result -> num_rows;
+$randNum = rand(0, $numRows - 1);
+
+$query = "SELECT url FROM BonusContent WHERE idx =" . $randNum . ";";
+$result = $mysqli -> query($query);
+
+$link = $result -> fetch_assoc();
+echo $link["url"]; // Yes, it is an array, even though it has only 1 thing
+
+$mysqli -> close();
 
 ?>
